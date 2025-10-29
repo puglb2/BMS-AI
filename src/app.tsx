ccd084d659cc5bb26f97ed25362b566f428b0615
puglb2 <puglb2@gmail.com>
2025-10-29 11:53:24 -0700

    Change root element for React app rendering

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
-const el = document.getElementById('ams-intake-chat')!
-createRoot(el).render(<app />)
+
+const el = document.getElementById('bms-ai-root')!
+createRoot(el).render(<App />)
